9d1a12e348e450474c8503afb82f5f3d37bf3856
david <kotalik.d@email.cz>
2017-06-11 22:05:35 +0200

    Delegate class

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 import os
 import sys
 from PySide import QtGui
+from PySide import QtCore
 
 class TodoItem:
 
@@ -26,12 +27,29 @@ class TodoList:
         for item in self.arr_todoList:
             item.printSelf()
 
+class MyListModel(QtCore.QAbstractListModel):
+	def __init__(self, parent=None,*args):
+		QtCore.QAbstractListModel.__init__(self, parent, *args)
+	
+	def rowCount(self, parent=QtCore.QModelIndex()):
+		return 1
+
+	def data(self, index, role):
+		return 1
+
+class ItemDelegate(QtGui.QItemDelegate):
+	
+
+
 class MainWindow(QtGui.QWidget):
     def __init__(self, parent=None):
 	super(MainWindow, self).__init__(parent)
 	self.initialize()
 
     def initialize(self):
+
+	self.todoList = TodoList()
+
 	grid = QtGui.QGridLayout()
 	grid.setSpacing(10)
 
@@ -42,8 +60,7 @@ class MainWindow(QtGui.QWidget):
 	btn_addItem.clicked.connect(self.addItem)
 	grid.addWidget(btn_addItem, 0, 2)
 
-	self.todoModel = QtGui.QStandardItemModel(0, 2)
-	self.todoModel.setHorizontalHeaderLabels(['Item','Time'])
+	self.todoModel = MyListModel(self)
 
 	btn_deleteItem = QtGui.QPushButton("Delete item", self)
 	grid.addWidget(btn_deleteItem, 1, 0)
@@ -54,7 +71,7 @@ class MainWindow(QtGui.QWidget):
 	btn_timeStop = QtGui.QPushButton("Stop time", self)
 	grid.addWidget(btn_timeStop, 1, 2)
 
-	self.lst_todo = QtGui.QTableView()
+	self.lst_todo = QtGui.QListView()
 	self.lst_todo.setModel(self.todoModel)
 	grid.addWidget(self.lst_todo, 2, 0, 5, 3)
 
@@ -63,7 +80,7 @@ class MainWindow(QtGui.QWidget):
 	self.show()
 
     def addItem(self):
-	self.lst_todo.append(TodoItem(self.txt_todoEntry.text()))
+	self.todoModel.appendRow(QtGui.QStandardItem("test"))
 
 def display_title_bar():
     os.system('clear')
